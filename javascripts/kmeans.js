44f1767040abb9bf0b2209a1abc3eca9b3de207b
mkfsn <p408865@gmail.com>
2014-03-22 11:59:59 +0800

    fixed wrong variable name

--- javascripts/kmeans.js
+++ javascripts/kmeans.js
@@ -8,7 +8,7 @@ function kmeans(k, points) {
 
   /* Compute distance of two points */
   var distance = function ( p1, p2 ){
-    return Math.sqrt( Math.pow((p1.x-p2.x),2) + Math.pow((p1.y1-p2.y), 2) );
+    return Math.sqrt( Math.pow((p1.x-p2.x),2) + Math.pow((p1.y-p2.y), 2) );
   }
 
   /* Random pick n points */
