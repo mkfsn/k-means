817003be2e3e6d8399e87903045e0c138c1f831a
mkfsn <p408865@gmail.com>
2014-03-22 11:47:18 +0800

    acomplete k-means main function

--- javascripts/kmeans.js
+++ javascripts/kmeans.js
@@ -4,12 +4,14 @@ function kmeans(k, points) {
   /* member data */
   var _k      = k || 2;
   var _points = points || [];
+  var _group  = new Array(_k);;
 
+  /* Compute distance of two points */
   var distance = function ( p1, p2 ){
     return Math.sqrt( Math.pow((p1.x-p2.x),2) + Math.pow((p1.y1-p2.y), 2) );
   }
 
-  /* random pick n points */
+  /* Random pick n points */
   var random_sample = function ( points ) {
     var sample = [];
     for ( var i = 0 ; i < _k && i < points.length ; i++ ) {
@@ -20,14 +22,33 @@ function kmeans(k, points) {
       }
       sample.push( points[random_number] );
     }
-    console.log( sample );
+    return sample;
   }
 
+  var mean = function ( points ) {
+    var x = 0.0, y = 0.0;
+    for ( i in points ) {
+      x += points[i].x;
+      y += points[i].y;
+    }
+    return { x: x/points.length, y: y/points.length };
 
+  /* Just a main function */
   var main = function () {
-    random_sample( _points );
-    return _points;
+    var mean_points = random_sample( _points );
+    {
+      for ( var i = 0 ; i < _k ; i++ ) {
+        _group[i] = [];
+      }
+      for ( var i = 0 ; i < _points.length ; i++ ) {
+        var t = new Array(_k);
+        for ( var j = 0 ; j < _k ; j++ ) {
+          t[j] = distance( _points[i], mean_points[j] );
+        }
+        _group[t.indexOf(Math.min(t))].append( _points[i] );
+      }
+    }
+    return _group;
   }
-
   return main( points );
 };
