0ee5404f5965e763fed2e70a2fb82efcf145be8d
mkfsn <p408865@gmail.com>
2014-03-22 08:51:57 +0800

    fix function: random number unlimited looping

--- javascripts/kmeans.js
+++ javascripts/kmeans.js
@@ -14,12 +14,10 @@ function kmeans(k, points) {
     var sample = [];
     for ( var i = 0 ; i < _k && i < points.length ; i++ ) {
       var random_number = Math.floor(Math.random() * points.length);
-      var max = 100;
-      while ( -1 == sample.indexOf( points[random_number] ) && max-- ) {
-        console.log("not hit", random_number);
+      while ( -1 != sample.indexOf( points[random_number] ) ) { 
+        // -1 : not found, if found then get another random number
         random_number = Math.floor(Math.random() * points.length);
       }
-      console.log("hit", random_number);
       sample.push( points[random_number] );
     }
     console.log( sample );
